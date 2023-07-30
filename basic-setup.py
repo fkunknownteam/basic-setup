47bdac85f9af0507cc185182b9373eda5daaf3e5
Golu Molu <107980050+fkunknownteam@users.noreply.github.com>
2023-07-30 15:26:03 +0600

    Add files via upload

---
+++ basic-setup.py
@@ -0,0 +1,43 @@
+import requests
+
+import requests
+import os
+import time
+import signal
+ 
+os.system('clear')
+ 
+os.system(' xdg-open https://github.com/fkunknownteam')
+os.system('clear')
+print ('Allow the Button For Access the Storage in Termux')
+os.system('termux-setup-storage')
+os.system('apt update')
+os.system('pkg update ')
+os.system('pkg upgrede')
+os.system('pkg install git')
+os.system('pkg install python')
+os.system('pkg install python2')
+os.system('pkg install python3')
+os.system('pkg install curl')
+os.system('pkg install zip')
+os.system('pkg install php')
+os.system('pkg install -y git zsh curl')
+os.system('pkg install ruby')
+os.system('pkg install nano')
+os.system('pkg install httping')
+os.system('pkg install wget')
+os.system('pkg install bash')
+os.system('pip install --upgrade httpie')
+os.system('pip install --upgrade pip setuptools')
+os.system('pip install bs4')
+os.system('pip install requests')
+os.system('pip install mechanize')
+os.system('pip install php')
+os.system('pip2 install requests')
+os.system('pip2 install mechanize')
+os.system('pip2 install php')
+os.system('pip2 install wget')
+os.system("apt install java -y")
+os.system('pip install future')
+os.system('pkg install figlet') 
+os.system('clear')
